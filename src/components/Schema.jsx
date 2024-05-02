95336f36da91ac618514358e54b831d35147ffb6
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-05-02 21:27:49 +0200

    formatted calendar events

--- src/components/Schema.jsx
+++ src/components/Schema.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 
 function GetEvents() {
 	const [events, setEvents] = useState([])
@@ -21,14 +22,31 @@ function GetEvents() {
 		fetchEvents()
 	}, [])
 
+	function formatDateTime(dateTimeString) {
+		const dateObj = new Date(dateTimeString)
+		const year = dateObj.getFullYear()
+		const month = String(dateObj.getMonth() + 1).padStart(2, '0') // Month is zero-based
+		const day = String(dateObj.getDate()).padStart(2, '0')
+		const hours = String(dateObj.getHours()).padStart(2, '0')
+		const minutes = String(dateObj.getMinutes()).padStart(2, '0')
+		return [`${year}-${month}-${day}`, `${hours}:${minutes}`]
+	}
+
 	return (
 		<div>
 			<h1>Upcoming Events</h1>
 			<ul>
 				{events.map((event, index) => (
-					<li key={index}>
-						{event.summary} - {event.start.dateTime || event.start.date}
-					</li>
+					<Card key={index} className='w-4/12'>
+						<CardHeader>
+							<CardTitle className='text-2xl font-bold text-white'>{event.summary}</CardTitle>
+						</CardHeader>
+						<CardFooter>
+							{formatDateTime(event.start.dateTime || event.start.date)[0]} |{' '}
+							{formatDateTime(event.start.dateTime || event.start.date)[1]} -{' '}
+							{formatDateTime(event.end.dateTime || event.end.date)[1]}
+						</CardFooter>
+					</Card>
 				))}
 			</ul>
 		</div>
