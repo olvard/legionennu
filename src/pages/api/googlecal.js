56c2a42b202a3e1f95d660c3e94fd9df65262978
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-05-05 15:16:09 +0200

    working graphic schedual

--- src/pages/api/googlecal.js
+++ src/pages/api/googlecal.js
@@ -36,7 +36,7 @@ async function listEvents(auth) {
 	const calendar = google.calendar({ version: 'v3', auth })
 	const res = await calendar.events.list({
 		calendarId: 'b3c47233db7d035dfbd4d0052f7224bbfb63cefd6de0d9d72a0c11123141913f@group.calendar.google.com',
-		maxResults: 10,
+		maxResults: 25,
 		singleEvents: true,
 		orderBy: 'startTime',
 	})
