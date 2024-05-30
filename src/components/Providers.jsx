4380f6785d3cf5cf00e178279bd5bd6a88caef47
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-05-30 11:38:12 +0200

    removed providers in pages

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -2,6 +2,6 @@
 
 import { ParallaxProvider } from 'react-scroll-parallax'
 
-export function Providers({ children }) {
+export default function Providers({ children }) {
 	return <ParallaxProvider>{children}</ParallaxProvider>
 }
