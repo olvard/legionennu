af7d7bcfe23c1fa7eebb96f2c2567752405236f4
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-06-07 22:30:05 +0200

    added spline 3d model

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,14 +1,10 @@
 import { Card, CardContent } from '@/components/ui/card'
+import Spline from '@splinetool/react-spline'
 
 export default function Divider() {
 	return (
-		<Card className='w-10/12 sm:w-8/12 h-52 overflow-clip flex  space-x-36'>
-			<img src='head.png' alt='pineapple'></img>
-			<img src='head.png' alt='pineapple'></img>
-			<img src='head.png' alt='pineapple'></img>
-			<img src='head.png' alt='pineapple'></img>
-			<img src='head.png' alt='pineapple'></img>
-			<img src='head.png' alt='pineapple'></img>
+		<Card className='w-10/12 sm:w-8/12 h-52 overflow-clip sm:flex space-x-36 hidden'>
+			<Spline scene='https://prod.spline.design/t6fYnNwXRhiEDXDi/scene.splinecode' />
 		</Card>
 	)
 }
