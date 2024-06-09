d81974c2b4b2f6b2e34e168de351c1471de4d3db
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-06-09 14:52:09 +0200

    fixed spons and responsiveness on schema

--- src/components/Spons.jsx
+++ src/components/Spons.jsx
@@ -5,8 +5,8 @@ export default function Spons() {
 		<div className='w-11/12 pb-8'>
 			<Card className='h-56 border-0 shadow-none bg-orange-100 flex items-center justify-between'>
 				<img src='strommen.svg' alt='Strömmen' className='w-1/4 h-1/3 md:h-2/3 mx-0' />
-				<img src='skylt.png' alt='skylt' className='w-1/4 ' />
-				<img src='korps.png' alt='korps' className='w-1/4  mr-4 animate-fade' />
+				<img src='skylt.png' alt='skylt' className='w-1/4 animate-fade' />
+				<img src='korps.png' alt='korps' className='w-1/4  mr-4 ' />
 			</Card>
 		</div>
 	)
