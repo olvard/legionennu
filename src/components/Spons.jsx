184ce85482b7e2d7db4e10cb4d9ab316fedcd5fc
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-06-18 13:22:39 +0200

    Made spons random

--- src/components/Spons.jsx
+++ src/components/Spons.jsx
@@ -1,13 +1,28 @@
-import { Card } from './ui/card'
+import { useEffect, useState } from 'react';
+import { Card } from './ui/card';
 
 export default function Spons() {
-	return (
-		<div className='w-11/12 pb-8'>
-			<Card className='h-56 border-0 shadow-none bg-orange-100 flex items-center justify-between'>
-				<img src='strommen.svg' alt='Strömmen' className='w-1/4 h-1/3 md:h-2/3 mx-0' />
-				<img src='skylt.png' alt='skylt' className='w-1/4 animate-fade' />
-				<img src='korps.png' alt='korps' className='w-1/4  mr-4 ' />
-			</Card>
-		</div>
-	)
+  const [showSkylt, setShowSkylt] = useState(false);
+
+  useEffect(() => {
+    // Generate a random number between 0 and 1
+    const randomNumber = Math.random();
+    // Set showSkylt to true if the random number is greater than 0.5
+    if (randomNumber > 0.5) {
+      setShowSkylt(true);
+    }
+  }, []);
+
+  return (
+    <div className='w-11/12 pb-8'>
+      <Card className='h-56 border-0 shadow-none bg-orange-100 flex items-center gap-20'>
+        <img src='strommen.svg' alt='Strömmen' className='w-1/4 h-1/3 md:h-2/3 mx-0' />
+        <img src='korps.png' alt='korps' className='w-1/4' />
+        {showSkylt && (
+          <img src='skylt.png' alt='skylt' className='w-1/4 mr-8' />
+        )}
+      </Card>
+    </div>
+  );
 }
+
