2a40b0964b7fdf433e7da793ac231412f50e28cf
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-07-07 12:25:26 +0200

    Big change

--- src/components/HejNollan.jsx
+++ src/components/HejNollan.jsx
@@ -2,23 +2,29 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 
 export default function HejNollan() {
 	return (
-		<Card className='w-10/12 sm:w-8/12 bg-gradient-to-r from-red-700 to-red-800'>
+		<Card className='w-10/12 sm:w-8/12 bg-gradient-to-r from-red-700 to-red-800 border-double '>
 			<CardHeader>
 				<CardTitle className='text-5xl font-bold text-white'>
-					Hej{' '}
+					Nämen hej{' '}
 					<span className='bg-gradient-to-r from-red-400 via-red-400 to-red-500 inline-block text-transparent bg-clip-text'>
 						Nollan!
 					</span>
 				</CardTitle>
 			</CardHeader>
 			<CardContent>
-				<p className='font-light text-white'>
-					Vad kul att du har hittat hit. Här på hemsidan kan du hitta spännande grejer och nödvändig
+				<p className='font-regular text-white'>
+					Välkommen till Linköpings universitet och Nolle-P 2024! De första två veckorna kallas för Nolle-P
+					och under denna tid kommer du att få lära känna universitetet, staden och dina nya vänner. Legionen
+					är här för att se till att du får en så bra start på studietiden som möjligt. Denna sida innehåller
+					schemat för alla aktiviteterna under Nolle-P, samt annat spännande.
+					<br></br>
+					<br></br>Vi ses snart, kanske.
+					{/* Vad kul att du har hittat hit. Här på hemsidan kan du hitta spännande grejer och nödvändig
 					information // kring vad Legionen hittar på. Legionen vill se till så att du får en så bra start på
 					universitetet som // möjligt när det kommer till både plugg och studentliv. Har Nollan tappat bort
 					eller glömt sin nollebok, // ingen fara, här på hemsidan finns en minst lika fin bok i digitalt
 					format. På den här sidan finns också // ett schema med tider och platser för aktiviteterna under
-					Nolle-P.Vi ses snart, kanske.
+					Nolle-P.Vi ses snart, kanske. */}
 				</p>
 			</CardContent>
 		</Card>
--- src/components/Nolleboken.jsx
+++ src/components/Nolleboken.jsx
@@ -8,22 +8,18 @@ export default function Nolleboken() {
 					<CardTitle className='text-5xl font-bold text-white'>Nolleboken</CardTitle>
 				</CardHeader>
 				<CardContent className='flex text-white'>
-					<p className='opacity-50'>
-						Här kan Nollan läsa en digital version av nolleboken, om det är så att nollan tappat bort sin
-						Nollebok, eller bara är sugen på att läsa den igen. Oroa dig icke, den är minst lika snygg som
-						den tryckta boken!
-					</p>
+					<p className='opacity-70'>Här kan Nollan läsa en digital version av nolleboken.</p>
 				</CardContent>
 				<CardFooter>Tryck på boken för att öppna den!</CardFooter>
 				<div className='flex'>
-					<a href='nollebokgdk.pdf' target='_blank' rel='noreferrer' className='block w-1/2 h-fit'>
+					<a href='nollebok_gdk_webb.pdf' target='_blank' rel='noreferrer' className='block w-1/2 h-fit'>
 						<img
 							className='w-56 h-1/2  transition duration-150 ease-in-out hover:scale-125 hover:cursor-pointer ml-auto mr-auto'
 							src='gdkbook.svg'
 							alt='book'
 						></img>
 					</a>
-					<a href='nollebokmt.pdf' target='_blank' rel='noreferrer' className='block w-1/2 h-fit'>
+					<a href='nollebok_mt_webb.pdf' target='_blank' rel='noreferrer' className='block w-1/2 h-fit'>
 						<img
 							className='w-56  h-1/2  transition duration-150 ease-in-out hover:scale-125 hover:cursor-pointer ml-auto mr-auto'
 							src='mtbook.svg'
