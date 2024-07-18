5099d4dbe9780037a7e1b69a23ae7f2dd6dd0d97
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-07-18 16:32:13 +0200

    Scroll background for legenden wip

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,107 @@
+export default function Scroll() {
+	return (
+		<svg
+			viewBox='0 0 128 128'
+			xmlns='http://www.w3.org/2000/svg'
+			xmlnsXlink='http://www.w3.org/1999/xlink'
+			aria-hidden='true'
+			role='img'
+			className='iconify iconify--noto w-9/12 absolute mx-auto'
+			preserveAspectRatio='xMidYMid meet'
+			fill='#000000'
+		>
+			<g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+			<g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+			<g id='SVGRepo_iconCarrier'>
+				{' '}
+				<path fill='#d68b52' d='M22.13 18.65h23.19v15.08H22.13z'>
+					{' '}
+				</path>{' '}
+				<ellipse cx='114.59' cy='108.83' rx='9.41' ry='12.65' fill='#f2bd72'>
+					{' '}
+				</ellipse>{' '}
+				<path
+					d='M114.59 96.18h-5.18s.04-63.41.04-74.11s-7.61-15.55-10.42-15.55h-76.9s10.1 1.21 10.23 15.55c.09 10.21 0 62.8 0 79.61s6.37 19.79 6.37 19.79h75.38c.07 0 .14-.01.21-.02c.09 0 .18.02.27.02c5.2 0 9.41-5.66 9.41-12.65c0-6.97-4.21-12.64-9.41-12.64z'
+					fill='#ffd8a1'
+				>
+					{' '}
+				</path>{' '}
+				<path
+					d='M114.59 96.18h-5.18s.04-63.41.04-74.11s-7.61-15.55-10.42-15.55h-76.9s10.1 1.21 10.23 15.55c.09 10.21 0 62.8 0 79.61s6.37 19.79 6.37 19.79h75.38c.07 0 .14-.01.21-.02c.09 0 .18.02.27.02c5.2 0 9.41-5.66 9.41-12.65c0-6.97-4.21-12.64-9.41-12.64z'
+					fill='#ffd8a1'
+				>
+					{' '}
+				</path>{' '}
+				<path d='M32.26 100.75l.09-9.1s59.53 6.46 77.04-14.12v22.99l-77.13.23z' fill='#f2bd72'>
+					{' '}
+				</path>{' '}
+				<g opacity='.6' fill='#9e673c' stroke-width='1' className='font-serif text-[3px]'>
+					<text x='40' y='20'>
+						För länge länge länge länge länge sedan, innan människorna och till{' '}
+						<tspan x='40' dy='1.2em'>
+							och med innan dinosaurierna, fanns det bara planeter och solar.
+						</tspan>{' '}
+						Solarna både värmde och lyste upp universums alla håll och kanter, men en dag hände någonting
+						märkligt. Den största solen på flera ljusårs radie började mullra och skaka för att tillslut
+						upplösas i ett enormt skådespel av flammor och strålar. Ur denna spektakulära supernova tog 11
+						skepnader form. Legionen hade anlänt till vår värld.
+					</text>
+				</g>{' '}
+				<path d='M38.25 96.18v6.17h84.41c-1.64-3.69-4.64-6.17-8.07-6.17H38.25z' fill='#fcebcd'>
+					{' '}
+				</path>{' '}
+				<ellipse cx='38.73' cy='108.83' rx='9.41' ry='12.65' fill='#f2bd72'>
+					{' '}
+				</ellipse>{' '}
+				<path
+					d='M45.71 108.17c0 5.33-3.34 8.35-6.92 9.33c-4.61 1.26-6.53.16-6.53.16V98.68s3.25-.82 6.72.16c3.54.99 6.73 4 6.73 9.33z'
+					fill='#784d30'
+				>
+					{' '}
+				</path>{' '}
+				<ellipse cx='33.48' cy='108.17' rx='6.72' ry='9.65' fill='#784d30'>
+					{' '}
+				</ellipse>{' '}
+				<ellipse cx='33.48' cy='108.18' rx='6.72' ry='9.65' fill='#784d30'>
+					{' '}
+				</ellipse>{' '}
+				<ellipse cx='24.86' cy='108.18' rx='4.46' ry='4.5' fill='#ba793e'>
+					{' '}
+				</ellipse>{' '}
+				<path
+					d='M33.02 105.44c1.27.39 2.82 3.08.18 5.66c-.67.65.02 2.09.48 3.3c.58 1.54 1 2.37 2.34 1.45c1.8-1.22 3.51-4.71 3.51-7.67c0-4.79-2.71-8.68-6.05-8.68c-1.72 0-3.06.55-4.19 2.88c-.5 1.04-.55 2.37 1.04 2.53c0 .01 1.78.26 2.69.53z'
+					fill='#ba793e'
+				>
+					{' '}
+				</path>{' '}
+				<ellipse cx='22.34' cy='20.09' rx='10.09' ry='13.57' fill='#f2bd72'>
+					{' '}
+				</ellipse>{' '}
+				<g>
+					{' '}
+					<path
+						d='M29.32 19.43c0 5.33-3.34 8.35-6.92 9.33c-4.61 1.26-6.53.16-6.53.16V9.94s3.25-.82 6.72.16c3.53.98 6.73 4 6.73 9.33z'
+						fill='#784d30'
+					>
+						{' '}
+					</path>{' '}
+					<ellipse cx='17.09' cy='19.43' rx='6.72' ry='9.65' fill='#784d30'>
+						{' '}
+					</ellipse>{' '}
+					<ellipse cx='17.09' cy='19.44' rx='6.72' ry='9.65' fill='#784d30'>
+						{' '}
+					</ellipse>{' '}
+					<ellipse cx='8.46' cy='19.44' rx='4.46' ry='4.5' fill='#ba793e'>
+						{' '}
+					</ellipse>{' '}
+					<path
+						d='M16.62 16.7c1.27.39 2.82 3.08.18 5.66c-.67.65.02 2.09.48 3.3c.58 1.54 1 2.37 2.34 1.45c1.8-1.22 3.51-4.71 3.51-7.67c0-4.79-2.71-8.68-6.05-8.68c-1.72 0-3.06.55-4.19 2.88c-.5 1.04-.55 2.37 1.04 2.53c0 .01 1.79.25 2.69.53z'
+						fill='#ba793e'
+					>
+						{' '}
+					</path>{' '}
+				</g>{' '}
+			</g>
+		</svg>
+	)
+}
