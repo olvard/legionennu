106b8c91eaa2d55696502cba9456c4dc72920932
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-07-23 23:19:58 +0200

    add iframe for utube

--- src/components/Legenden.jsx
+++ src/components/Legenden.jsx
@@ -6,6 +6,8 @@ export default function Legenden() {
 			<h1 className='font-serif text-5xl font-bold text-orange-100 mb-4 '>LEGENDEN</h1>
 
 			<img src='scroll.png' alt='legenden' className='w-full sm:w-9/12 mx-auto ' />
+
+			<iframe className='w-8/12 h-96 sm:h-[579px] mt-8' src='https://www.youtube.com/embed/Fzp8Wku-_aU'></iframe>
 		</div>
 	)
 }
