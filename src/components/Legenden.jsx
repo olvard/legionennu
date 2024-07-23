6b2d909c480597a8aa2e4658ba2c119ed608811a
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2024-07-23 23:14:43 +0200

    add png

--- src/components/Legenden.jsx
+++ src/components/Legenden.jsx
@@ -5,7 +5,7 @@ export default function Legenden() {
 		<div className='w-full flex justify-center items-center flex-col'>
 			<h1 className='font-serif text-5xl font-bold text-orange-100 mb-4 '>LEGENDEN</h1>
 
-			<img src='scroll.svg' alt='legenden' className='w-full sm:w-9/12 mx-auto ' />
+			<img src='scroll.png' alt='legenden' className='w-full sm:w-9/12 mx-auto ' />
 		</div>
 	)
 }
